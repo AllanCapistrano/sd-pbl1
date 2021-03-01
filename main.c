67800c378148462683dd58f7cdc624abc14a1725
JoaoErick <jerick1415@outlook.com>
2021-03-01 13:29:45 -0300

    Matriz funcionando

--- main.c
+++ main.c
@@ -2,50 +2,81 @@
 #include<stdlib.h>
 #include <time.h>
 
-// Função que realiza a leitura do arquivo
-long int readArq(long int **matrizA, long int **matrizB) {
-	// Declarando variáveis
-	int i, j;
-	long int size = 0;
-	
-	// Declarando as variáveis ponteiro para os arquivos
-	FILE *file;
-	
-	// Abrindo o arquivo para leitura
-	file = fopen("matrizes.txt","r");
-	// Lendo a quantidade de elementos por linha
-	fscanf(file,"%ld",&size);
-    
-    // Realocando o tamanho das matrizes
-    matrizA = (long int**) realloc(matrizA, (size - 1) * sizeof(long int*));
-	matrizB = (long int**) realloc(matrizB, (size - 1) * sizeof(long int*));
-	
-	for (i = 0; i < size; i++) {
-		matrizA[i] = malloc(size * sizeof(long int));
-		matrizB[i] = malloc(size * sizeof(long int));	
-	}
+// Função para alocar dinamicamente o tamanho da matriz
+long int **AlocaMatriz(long int size){
+  long int **M;
+  int i;
 
-	// Lendo o arquivo e salvando os valores nas matrizes
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++){
-			fscanf(file, "%ld", &matrizA[i][j]);
-			printf("\nmatrizA[%d][%d]: %ld\n", i, j, matrizA[i][j]);
-		}
-	}
-	
-	printf("\n---\n");
+  M = (long int**)malloc(sizeof(long int*) * size);
+  if(M == NULL){
+    printf("Memoria insuficiente.\n");
+    exit(1);
+  }
+  for(i = 0; i < size; i++){
+    M[i] = (long int *)malloc(sizeof(long int) * size);
+    if(M[i] == NULL){
+      printf("Memoria insuficiente.\n");
+      exit(1);
+    }
+  }
+  return M;
+}
 
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++){
-			fscanf(file, "%ld", &matrizB[i][j]);
-			printf("\nmatrizB[%d][%d]: %ld\n", i, j, matrizB[i][j]);
-		}
-	}
-	
-	// Fechando o arquivo de leitura
-	fclose(file);
-	
-	return size;
+// Função para liberar o espaço na memória que foi alocado dinâmicamente
+void LiberaMatriz(long int **M, long int size){
+  int i;
+  for(i = 0; i < size; i++)
+    free(M[i]);
+  free(M);
+}
+
+// Função para a ler a quantidade de elementos das matrizes no arquivo
+void LeTamanho(long int *size){
+	FILE *fp;
+  	long int sizef;
+  	
+  	fp = fopen("matrizes.txt", "r");
+  	if(fp == NULL){
+	    printf("Erro na leitura do arquivo.\n");
+	    exit(1);
+  	}
+  
+  	fscanf(fp,"%ld",&sizef);
+  	*size = sizef;
+  	
+  fclose(fp);
+}
+
+// Função para ler o conteúdo das matrizes no arquivo
+void LeMatriz(long int **matrizA, long int **matrizB,long int size){
+  long int **M, **M2;
+  FILE *fp;
+  long int i,j;
+ 
+  fp = fopen("matrizes.txt", "r");
+  if(fp == NULL){
+    printf("Erro na leitura do arquivo.\n");
+    exit(1);
+  }
+  
+  //Desloca o cursor à frente do arquivo de texto em número de bytes correspondente ao long int, partindo da posição atual.
+  fseek(fp, +sizeof(long int), SEEK_CUR);
+  
+  M = AlocaMatriz(size);
+  M2 = AlocaMatriz(size);
+  for(i = 0; i < size; i++){
+    for(j = 0; j < size; j++){
+      fscanf(fp,"%ld",&M[i][j]);
+      matrizA[i][j] = M[i][j];
+    }
+  }
+  for(i = 0; i < size; i++){
+    for(j = 0; j < size; j++){
+      fscanf(fp,"%ld",&M2[i][j]);
+      matrizB[i][j] = M2[i][j];
+    }
+  }
+  fclose(fp);
 }
 
 int main()
@@ -55,33 +86,25 @@ int main()
 	
     // Declarando variáveis
     long int **matrizA, **matrizB, **matrizC;
-    long int size, aux = 0;
+    long int size;
     double timeSpent;
-	int i, j, k;
+	int aux = 0, i, j, k;
 	
 	// Declarando as variáveis ponteiro para os arquivos
     FILE *file2;
-
-	// Alocando dinamicamente o tamanho das matrizes
-	matrizA = malloc(1 * sizeof(long int*));
-	matrizB = malloc(1 * sizeof(long int*));
 	
-//	for(i = 0; i < 1; i++) {
-		matrizA[0] = malloc(1 * sizeof(long int));
-		matrizB[0] = malloc(1 * sizeof(long int));
-//	}
-
-	// Chamando a função para a leitura do arquivo
-	size = readArq(matrizA, matrizB);
+	// Chamando a função para a ler a quantidade de elementos das matrizes no arquivo
+	LeTamanho(&size);
+	
+	// Alocando dinamicamente o tamanho das matrizes
+	matrizA = AlocaMatriz(size);
+  	matrizB = AlocaMatriz(size);
+  	
+  	// Chamando a função para ler o conteúdo das matrizes no arquivo
+	LeMatriz(matrizA,matrizB, size);
 	
 	// Alocando dinamicamente a matriz resultante
-	matrizC = malloc(size * sizeof(long int*));
-	
-	for(i = 0; i < size; i++) {
-		matrizC[i] = malloc(size * sizeof(long int));
-	}
-	
-	printf("\nMatrizA: %ld\n", matrizB[0][8]);
+	matrizC = AlocaMatriz(size);
 	
 	// Multiplicando as matrizes
 	for(i = 0; i < size; i++) {
@@ -97,8 +120,6 @@ int main()
 		}
 	}
 	
-	printf("\nc\n", size);
-	
 	// Abrindo o arquivo para as escrita da matriz resultante
 	file2 = fopen("matriz-resultante.txt", "w");
 	
@@ -111,11 +132,6 @@ int main()
 		fprintf(file2, "\n");
 	}
 	
-	// Libera a memória da matriz
-	free (matrizA);
-	free (matrizB);
-	free (matrizC);
-	
 	// Finalizando a contagem do tempo de execução
 	clock_t end = clock();
 	
@@ -125,7 +141,12 @@ int main()
 	// Exibindo o tempo de execução
 	printf("Tempo de Execucao: %.3lf s\n\n", timeSpent);
 	
-	// Fechando o arquivo de escrita
+	// Libera a memória da matriz
+	LiberaMatriz(matrizA, size);
+  	LiberaMatriz(matrizB, size);
+  	LiberaMatriz(matrizC, size);
+  	
+  	// Fechando o arquivo de escrita
 	fclose(file2);
 	
     system ("pause");
