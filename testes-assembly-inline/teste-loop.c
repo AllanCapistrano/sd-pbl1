04388abf5ddc24593f492aadbb9d6181e085aa31
AllanCapistrano <allan.capistrano3@gmail.com>
2021-03-08 18:09:17 -0300

    Teste de loop inline assembly.

---
+++ testes-assembly-inline/teste-loop.c
@@ -0,0 +1,32 @@
+/*---------------------------------------------*/
+/* No terminal:                                */
+/* $ gcc teste.c -o teste.exe -masm=intel      */
+/* $ ./teste.exe                               */
+/*---------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int loop();
+
+
+int main() {
+
+	printf("\nTeste Loop: %d\n", loop());
+	
+	system("pause");
+	return 0;
+}
+
+/* Assembly inline básico */
+asm(
+	"loop:\n"
+		"XOR    EAX, EAX\n"
+		"MOV	ECX, 500\n"   
+	"Label:\n"            
+		"INC     EAX\n"       
+		"LOOP     Label\n"
+		"ret"
+);
+
+
